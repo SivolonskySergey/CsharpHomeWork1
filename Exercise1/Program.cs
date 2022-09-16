21162e742c564c8d29f5ffc7d8dfcc7566b00d19
Sergey <bibert.shmibert@gmail.com>
2022-09-16 15:08:20 +0400

    add logic exercise1

--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿Console.WriteLine("Введите число 1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2) 
+{
+    Console.WriteLine($"{num1} больше чем {num2}!");
+}
+else if (num1 < num2)
+{
+    Console.WriteLine($"{num2} больше чем {num1}!");
+}
+else
+{
+    Console.WriteLine($"{num1} и {num2} равны!");
+}
