4bc3c28dc41db330e39c43f02ff75d04682d1ba8
Sergey <bibert.shmibert@gmail.com>
2022-09-16 17:26:28 +0400

    add exercise4

---
+++ Exercise4/Program.cs
@@ -0,0 +1,28 @@
+﻿bool isEven(int num) 
+{
+    if (num % 2 == 0)
+    {
+        return true;
+    }
+    else 
+    {
+        return false;
+    }
+};
+
+Console.WriteLine("Введите число больше нуля: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+if (number <= 0) {
+    Console.WriteLine("Число не соответствует условию!");
+}
+else {
+    while(result < number)
+    {
+        result++;
+        if(isEven(result)) 
+        {
+        Console.WriteLine(result);
+        }
+    }
+};
